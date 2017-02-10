f4a4d089866396b3dc71de406030df08ebbff894
Zach <zachraymer@gmail.com>
2017-02-09 20:31:18 -0600

    working on recursing through subranges

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var fs = require('fs');
 
 var readSource = './input.txt';
 
-// console.log("Reading file synchronously");
-// var fileData = fs.readFileSync(readSource, "utf8");
-// console.log(fileData);
-
 var inputArr = fs.readFileSync(readSource).toString().split("\n");
 console.log(inputArr)
 
@@ -13,13 +9,52 @@ var k;
 var n;
 var subArr = [];
 
+//this function just takes the text fromm the input file and asigns variables
 function parseTxt(arr) {
 	paramArr = inputArr[0].split(' ')
 	n = parseInt(paramArr[0]);
 	k = parseInt(paramArr[1]);
-	subArr = inputArr[1].split(' ');
+	subArr = inputArr[1].split(' ').map(function(num) {
+		return parseInt(num);
+	})
 }
 parseTxt(inputArr);
-console.log(n);
-console.log
+//call it on the input file
+console.log('this is n', n);
+console.log('this is k', k)
+console.log('this is sub array', subArr);
+
+function subRange(n,k,arr) {
+	if(arr.length<k){
+		return;
+	};
+	var incr = 0;
+	var decr = 0;
+	for(i=0; i<=k-1; i++){
+		if(arr[i]<arr[i+1]){
+			if(incr===0){
+				incr+=2
+			}else{
+				incr+=1
+			}
+		}
+		if(arr[i]<arr[i-1]){
+			if(decr===0){
+				decr+=2
+			}else{
+				decr+=1
+			}
+		}
+	}
+	console.log(incr-decr);
+	arr.splice(0,1);
+	console.log(arr);
+	subRange(n,k,arr);
+}
+
+subRange(n, k, subArr);
+
+
+
+
 
