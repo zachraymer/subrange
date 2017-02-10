f9179f0c14835189e3f6813fc106f8bac9982dce
Zach <zachraymer@gmail.com>
2017-02-10 00:26:58 -0600

    passes tests, fully functional

--- app.js
+++ app.js
@@ -19,64 +19,54 @@ function parseTxt(arr) {
 }
 parseTxt(inputArr);
 //call it on the input file
-console.log('this is n', n);
-console.log('this is k', k)
-console.log('this is sub array', subArr);
+// console.log('this is n', n);
+// console.log('this is k', k)
+// console.log('this is sub array', subArr);
 
-function findIncr(arr) {
-	count = 0;
-	for(i=0; i<arr.length-1; i++){
-		var increasing = false
-		for(j=i+1; j<arr.length; j++){
-			if(!arr[j]>arr[j-1]){
-				increasing=false
-				break
+function subRange(arr) {
+	if(arr.length<=1){
+		return 0
+	}else{
+		var increasing = true;
+		var decreasing = true;
+		for (var h=0; h<arr.length-1; h++){
+			if(arr[h]>=arr[h+1]){
+				increasing = false;
 			}
-			if(increasing){
-				count+=1;
-			}
-			if(arr[j]>arr[j-1]){
-				count+=1;
-				increasing=true
+			if(arr[h]<=arr[h+1]){
+				decreasing = false;
 			}
 		}
-	}
-	return count
-}
-
-function findDecr(arr) {
-	count = 0;
-	for(i=0; i<arr.length-1; i++){
-		var decreasing = false
-		for(j=i+1; j<arr.length; j++){
-			if(decreasing){
-				count+=1;
-			}
-			else if(arr[j]<arr[j-1]){
-				count+=1;
-				decreasing=true
-			}else{
-				decreasing=false
-			}
+		if(increasing){
+			return 1
+		}else if(decreasing){
+			return -1
+		}else{
+			return 0
 		}
 	}
-	return count
-}
-console.log('please work: ',findIncr([188930,194123,201345,154243]))
-
-
-function subRange(n,k,arr) {
-	if(arr.length<k){
-		return;
-	};
-	currSubRange = arr.slice(0,k)
-	console.log(currSubRange)
-	console.log(findIncr(currSubRange)-findDecr(currSubRange));
-	arr.splice(0,1);
-	subRange(n,k,arr);
 }
 
-// subRange(n, k, subArr);
+
+var partitions = n-k+1;
+
+function main(partition) {
+	for(var i=0; i<partition; i++){
+		var subWindow = subArr.slice(i,i+k)
+		var count = 0;
+		for(var j=2; j<=k; j++){
+			for(var g=0; g<=subWindow.length-j; g++){
+				count += subRange(subWindow.slice(g,j+g))
+			}
+		}
+		console.log(count);
+	}
+}
+
+main(partitions)
+
+
+
 
 
 
